bf78c3f086ccc6772f9dc4f31ef9562f40d5fc9c
Jon <jdog992@gmail.com>
2020-06-02 16:10:42 -0400

    finished

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const helmet =require('helmet');
+const helmet = require('helmet');
+const moviedex = require('./moviedex')
 
 const app = express();
 
@@ -9,7 +11,39 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('common'));
 
-app.get('/movie', (req, res) => {})
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json( { error: 'Unauthorized request' } )
+    }
+
+    next()
+})
+
+app.get('/movie', (req, res) => {
+    const {genre, country, avg_vote} = req.query;
+    let results = [...moviedex];
+    let voteNum = Number(avg_vote);
+    
+    if(voteNum === NaN) {
+        return res.status(400).json({ error: 'average vote must be a number'})
+    }
+        
+    if (genre) {
+        results = results.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase())
+        )
+    }
+    if (country) {
+        results = results.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()))
+    }
+    if (avg_vote) {
+        results = results.filter(movie => movie.avg_vote >= voteNum)
+    }
+
+    res.json(results)
+})
 
 const PORT = 8000;
 
