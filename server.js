e17919c9aed9734bae365f24eb2926b5ded9ca7d
Ryan Vargas <i.am.ryan.v@gmail.com>
2020-06-05 15:46:33 -0700

    Fixed for deployment

--- server.js
+++ server.js
@@ -1,52 +1,67 @@
+'use strict';
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const moviedex = require('./moviedex')
+const moviedex = require('./moviedex');
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(morgan('common'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json( { error: 'Unauthorized request' } )
-    }
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
 
-    next()
-})
+  next();
+});
 
 app.get('/movie', (req, res) => {
-    const {genre, country, avg_vote} = req.query;
-    let results = [...moviedex];
-    let voteNum = Number(avg_vote);
-    
-    if(voteNum === NaN) {
-        return res.status(400).json({ error: 'average vote must be a number'})
-    }
-        
-    if (genre) {
-        results = results.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase())
-        )
-    }
-    if (country) {
-        results = results.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()))
-    }
-    if (avg_vote) {
-        results = results.filter(movie => movie.avg_vote >= voteNum)
-    }
+  const { genre, country, avg_vote } = req.query;
+  let results = [...moviedex];
+  let voteNum = Number(avg_vote);
 
-    res.json(results)
-})
+  if (isNaN(voteNum)) {
+    return res.status(400).json({ error: 'average vote must be a number' });
+  }
 
-const PORT = 8000;
+  if (genre) {
+    results = results.filter((movie) =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+  if (country) {
+    results = results.filter((movie) =>
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+  if (avg_vote) {
+    results = results.filter((movie) => movie.avg_vote >= voteNum);
+  }
+
+  res.json(results);
+});
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log('Server started on PORT 8000')
-})
+  console.log('Server started on PORT 8000');
+});
