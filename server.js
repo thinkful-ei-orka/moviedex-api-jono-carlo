ccbb385a48d506929b25cd09d09f15e784aa4069
Ryan Vargas <i.am.ryan.v@gmail.com>
2020-06-05 16:10:01 -0700

    More tweaks

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(morgan(morganSetting));
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
+  console.log(apiToken);
+  console.log(authToken);
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
@@ -29,8 +31,10 @@ app.get('/movie', (req, res) => {
   let results = [...moviedex];
   let voteNum = Number(avg_vote);
 
-  if (isNaN(voteNum)) {
-    return res.status(400).json({ error: 'average vote must be a number' });
+  if (voteNum) {
+    if (isNaN(voteNum)) {
+      return res.status(400).json({ error: 'average vote must be a number' });
+    }
   }
 
   if (genre) {
